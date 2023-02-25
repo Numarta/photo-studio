a94488365bc09d4f4fe95dd2906b1e2740d58265
Numart <johnn.hotmail@mail.ru>
2023-02-25 14:20:01 +0300

    fix some bugs

--- test2/delimg.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start();
-    $rest = substr($_GET['name'], 6, strlen($_GET['name'])); // удаляет из названия "test2/"
-   // echo $rest;
-   unlink($rest);
-   header("Location:../print.html");
-    ?>
--- test2/delimgMug.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start();
-    $rest = substr($_GET['name'], 6, strlen($_GET['name']));
-   // echo $rest;
-   unlink($rest);
-   header("Location:../mugFoto.html");
-    ?>
